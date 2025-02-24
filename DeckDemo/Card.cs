ae731b67d102ed25dad329948ef69d87a02974de
anderscheng326 <anderscheng326@gmail.com>
2025-02-24 14:40:45 -0500

    implement card class and test case

--- DeckDemo/Card.cs
+++ DeckDemo/Card.cs
@@ -1,3 +1,21 @@
+/*
+UML
+Class: Card
+ Fields:
+    -rank:Rank
+    -suit:Suit
+    -isFaceUp:bool
+
+ Properties:
+    +rank:Rank
+    +suit:Suit
+
+ Methods:
+    +FlipOver():void
+
+ Constructor:
+    Card(){rank:Rank, suit:Suit}
+*/
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,18 +27,30 @@ public class Card
 {
     //Fields, example: Rank rank;
     //check the help documentation for the fields
-
+    Rank rank;
+    Suit suit;
+    bool isFaceUp;
 
     //Card Constructor
-
-
+    public Card(Rank rank, Suit suit)
+    {
+        this.suit = suit;
+        this.rank = rank;
+        this.isFaceUp = false;
+    }
+  
     //Define properties for all above fields
     //code example: public Suit Suit { get { return suit; } }
 
+    public Rank Rank{get {return rank;}}
+
+    public Suit Suit{get {return suit;}}
+
+    public bool IsFaceUp{get {return isFaceUp;}}
 
     public void FlipOver()
     {
-        //implementation 
+        isFaceUp = !isFaceUp;
     }
         
-}
+} 
--- DeckDemo/Program.cs
+++ DeckDemo/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Test
+{
+    public static void Main(string[] args)
+     {  
+        //test case for card class
+        Card card1 = new Card(Rank.Ace, Suit.Spades);
+        Console.WriteLine($"{card1.Rank} of {card1.Suit}");
+        Console.WriteLine($"FaceUp: {card1.IsFaceUp}");
+        Console.WriteLine("Flipping the card!");
+        card1.FlipOver();
+        Console.WriteLine($"FaceUp: {card1.IsFaceUp}");
+
+     }
+
+}
